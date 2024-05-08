db047b640063e20206512b4a075cd5460b84871f
phbrugnolo <phbrugnolo@gmail.com>
2024-05-08 13:22:12 -0300

    mainupulando strings

---
+++ e1/main.rb
@@ -0,0 +1,12 @@
+puts 'Hello World'
+print "Hello World\n"
+
+# print não quebra linha
+# puts = println em java 
+
+first_name = "Pedro"
+last_name = "Brugnolo"
+full_name = first_name + " " + last_name
+
+puts "Olá, " + full_name
+
