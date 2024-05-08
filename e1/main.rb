7456820ac555f8682a5160e882ce476acca6428d
phbrugnolo <phbrugnolo@gmail.com>
2024-05-08 14:16:34 -0300

    refactoring string folder

--- e1/main.rb
+++ e1/main.rb
@@ -3,10 +3,3 @@ print "Hello World\n"
 
 # print não quebra linha
 # puts = println em java 
-
-first_name = "Pedro"
-last_name = "Brugnolo"
-full_name = first_name + " " + last_name
-
-puts "Olá, " + full_name
-
---
+++ e2/string.rb
@@ -0,0 +1,5 @@
+first_name = "Pedro"
+last_name = "Brugnolo"
+full_name = first_name + " " + last_name
+
+puts "Olá, " + full_name
